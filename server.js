1829709900dcc13a1dc90a9f19f20f5c83d1055e
soraalam1 <s1gamingc@gmail.com>
2020-11-22 19:41:07 -0500

    capitalized banking api path

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static('./dist/banking-api-frontend'));
+app.use(express.static('./dist/BANKING-API-FRONTEND'));
 
 app.get('/*', function (req, res) {
-  res.sendFile('index.html', { root: 'dist/banking-api-frontend' }
+  res.sendFile('index.html', { root: 'dist/BANKING-API-FRONTEND' }
   );
 });
 
